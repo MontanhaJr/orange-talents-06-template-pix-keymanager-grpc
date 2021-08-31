de80782ad8d49a0470e2d9c484deb587368d5b95
Mauricio Montanha Junior <mauriciomontanhajunior@gmail.com>
2021-08-31 13:35:44 -0300

    Cadastro de chave pix finalizado

--- src/main/kotlin/br/com/zup/edu/chavePix/TipoDeChave.kt
+++ src/main/kotlin/br/com/zup/edu/chavePix/TipoDeChave.kt
@@ -1,6 +1,7 @@
 package br.com.zup.edu.chavePix
 
-import io.micronaut.validation.validator.constraints.EmailValidator
+
+import org.hibernate.validator.internal.constraintvalidators.hv.EmailValidator
 import org.hibernate.validator.internal.constraintvalidators.hv.br.CPFValidator
 
 
